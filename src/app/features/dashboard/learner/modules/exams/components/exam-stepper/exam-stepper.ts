102ba2ffd9d9472f984ea18a5fc2ad719d0144ef
esraaabuhalawa <esr.saad123@gmail.com>
2026-07-28 18:49:45 +0300

    update project structure

---
+++ src/app/features/dashboard/learner/modules/exams/components/exam-stepper/exam-stepper.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quiz-app-exam-stepper',
+  imports: [],
+  templateUrl: './exam-stepper.html',
+  styleUrl: './exam-stepper.scss',
+})
+export class ExamStepper {}
